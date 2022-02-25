b63686625fdd34bb05f949f1d4b4fe8e6a0eab07
David Wragg <david-a.wragg@db.com>
2022-02-25 11:15:45 +0000

    AAP-21559: install prettier and add config

---
+++ web/.prettierrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  arrowParens: 'always',
+  importOrder: [
+    'polyfills?$',
+    '<THIRD_PARTY_MODULES>',
+    '^@plexus',
+    '^[./](?!.*[.]s?css$).*$',
+    '.*css$',
+  ],
+  importOrderSeparation: true,
+  importOrderCaseInsensitive: true,
+  importOrderSortSpecifiers: true,
+  overrides: [
+    {
+      files: '*.ts',
+      options: {
+        printWidth: 120,
+      },
+    },
+  ],
+};
